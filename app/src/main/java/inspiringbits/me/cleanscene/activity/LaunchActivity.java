6634d4c751cd28798e2d6b3e6042947f69de8813
Ivan <jyf514@gmail.com>
2017-08-19 15:22:04 +1000

    bug fix

--- app/src/main/java/inspiringbits/me/cleanscene/activity/LaunchActivity.java
+++ app/src/main/java/inspiringbits/me/cleanscene/activity/LaunchActivity.java
@@ -1,12 +1,9 @@
 package inspiringbits.me.cleanscene.activity;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.os.CountDownTimer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -26,37 +23,20 @@ public class LaunchActivity extends Activity {
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
         super.onCreate(savedInstanceState);
-        new Timer(2000,1000,this::startMainPage).start();
+        new CountDownTimer(3000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                LaunchActivity.this. startActivity(new Intent(LaunchActivity.this,MainActivity.class));
+                LaunchActivity.this.finish();
+            }
+        }.start();
     }
 
-    private Integer startMainPage(CountDownTimer timer){
-        timer.cancel();
-        timer=null;
-        startActivity(new Intent(this,MainActivity.class));
-        finish();
-        return 0;
-    }
-
-    private class Timer extends CountDownTimer {
-
-        Function<CountDownTimer,Integer> callback;
-
-        public Timer(long millisInFuture, long countDownInterval,Function<CountDownTimer,Integer> callback) {
-            super(millisInFuture, countDownInterval);
-            this.callback=callback;
-        }
 
 
-        @Override
-        public void onTick(long millisUntilFinished) {
-
-        }
-
-        @Override
-        public void onFinish() {
-            callback.apply(this);
-
-
-        }
-    }
 }

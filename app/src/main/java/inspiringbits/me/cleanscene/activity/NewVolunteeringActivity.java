2fd528bcdaae45824c8d6d0e894ed1b261e9deee
Ivan <jyf514@gmail.com>
2017-08-26 23:18:39 +1000

    new volunteer activity page

--- app/src/main/java/inspiringbits/me/cleanscene/activity/NewVolunteeringActivity.java
+++ app/src/main/java/inspiringbits/me/cleanscene/activity/NewVolunteeringActivity.java
@@ -1,18 +1,26 @@
 package inspiringbits.me.cleanscene.activity;
 
 import android.Manifest;
-import android.app.ActionBar;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.githang.statusbar.StatusBarCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -30,7 +38,14 @@ import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import inspiringbits.me.cleanscene.R;
+import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class NewVolunteeringActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final String MAPVIEW_BUNDLE_KEY = "NewVolunteering";
@@ -38,14 +53,83 @@ public class NewVolunteeringActivity extends AppCompatActivity implements OnMapR
     MapView mMapView;
     FusedLocationProviderClient mFusedLocationClient;
     Marker marker;
+    @BindView(R.id.header)
+    ConstraintLayout headerLayout;
+    @BindView(R.id.hint_card)
+    CardView hintCard;
+    @BindView(R.id.address1)
+    TextView address1;
+    @BindView(R.id.address2)
+    TextView address2;
+    @BindView(R.id.more_info)
+    TextView moreInfo;
+    BottomSheetBehavior bsb;
+    @BindView(R.id.event_date)
+    EditText eventDate;
+    @BindView(R.id.meeting_point)
+    EditText meetingPoint;
+    @BindView(R.id.start_time)
+    EditText startTime;
+    @BindView(R.id.end_time)
+    EditText endTime;
+    @BindView(R.id.description)
+    EditText description;
+    @BindView(R.id.is_private)
+    CheckBox isPrivate;
+    @BindView(R.id.submit)
+    Button submmit;
+
+    @OnClick(R.id.more_info)
+    void showDetail(View v) {
+        if (bsb.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
+            bsb.setState(BottomSheetBehavior.STATE_EXPANDED);
+        } else {
+            bsb.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_volunteering);
-        android.support.v7.app.ActionBar bar = getSupportActionBar();
+        ActionBar bar = getSupportActionBar();
         bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#5E97DC")));
-        StatusBarCompat.setStatusBarColor(this, ContextCompat.getColor(this,R.color.status_bar));
+        StatusBarCompat.setStatusBarColor(this, ContextCompat.getColor(this, R.color.status_bar));
         ButterKnife.bind(this);
+
+        bsb = BottomSheetBehavior.from(findViewById(R.id.design_bottom_sheet));
+        bsb.setState(BottomSheetBehavior.STATE_HIDDEN);
+        ViewTreeObserver observer = headerLayout.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // TODO Auto-generated method stub
+                int headerLayoutHeight = headerLayout.getHeight();
+                bsb.setPeekHeight(headerLayoutHeight);
+                headerLayout.getViewTreeObserver().removeGlobalOnLayoutListener(
+                        this);
+            }
+        });
+        bsb.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        moreInfo.setText("SHOW MAP");
+                        hintCard.setVisibility(View.GONE);
+                        break;
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        moreInfo.setText("MORE INFO");
+                        hintCard.setVisibility(View.VISIBLE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         Bundle mapViewBundle = null;
@@ -58,9 +142,10 @@ public class NewVolunteeringActivity extends AppCompatActivity implements OnMapR
 
     @Override
     public void onMapReady(GoogleMap map) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             map.moveCamera(CameraUpdateFactory.zoomTo(15.0f));
-            map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-37.821202,144.957946)));
+            map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-37.821202, 144.957946)));
             return;
         }
         map.setMyLocationEnabled(true);
@@ -68,11 +153,11 @@ public class NewVolunteeringActivity extends AppCompatActivity implements OnMapR
                 .addOnSuccessListener(this, location -> {
                     LatLng myLatlng;
                     if (location != null) {
-                        myLatlng=new LatLng(location.getLatitude(),location.getLongitude());
+                        myLatlng = new LatLng(location.getLatitude(), location.getLongitude());
                         map.moveCamera(CameraUpdateFactory.zoomTo(15.0f));
                         map.moveCamera(CameraUpdateFactory.newLatLng(myLatlng));
-                    } else{
-                        myLatlng=new LatLng(-37.821202,144.957946);
+                    } else {
+                        myLatlng = new LatLng(-37.821202, 144.957946);
                         map.moveCamera(CameraUpdateFactory.zoomTo(15.0f));
                         map.moveCamera(CameraUpdateFactory.newLatLng(myLatlng));
                     }
@@ -80,34 +165,79 @@ public class NewVolunteeringActivity extends AppCompatActivity implements OnMapR
         map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-                if (marker!=null){
+                if (marker != null) {
                     marker.remove();
                 }
-                marker=map.addMarker(new MarkerOptions().position(latLng));
-                Log.d("location", "onMapLongClick: "+getLocationInformation(latLng));
+                marker = map.addMarker(new MarkerOptions().position(latLng));
+                //Log.d("location", "onMapLongClick: " + getLocationInformation(latLng));
+                Observable.create((ObservableOnSubscribe<String>) e -> {
+                    String address = getLocationInformation(latLng);
+                    e.onNext(address);
+                    e.onComplete();
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@io.reactivex.annotations.NonNull Disposable d) {
+                        
+                    }
+
+                    @Override
+                    public void onNext(@io.reactivex.annotations.NonNull String s) {
+                        if (s.equals("Unknown Address")) {
+                            address2.setText("Unknown");
+                            address1.setText("Unknown");
+                        } else {
+                            String[] addr = s.split(", ");
+                            address1.setText(addr[0]);
+                            address2.setText(addr[1]);
+                        }
+                        bsb.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    }
+
+                    @Override
+                    public void onError(@io.reactivex.annotations.NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+                /*String address = getLocationInformation(latLng);
+                if (address.equals("Unknown Address")) {
+                    address2.setText("Unknown");
+                    address1.setText("Unknown");
+                } else {
+                    String[] addr = address.split(",");
+                    address1.setText(addr[0]);
+                    address2.setText(addr[1]);
+                }
+                bsb.setState(BottomSheetBehavior.STATE_COLLAPSED);*/
             }
         });
     }
 
-    private String getLocationInformation(LatLng latLng){
+    private String getLocationInformation(LatLng latLng) {
         try {
 
             Geocoder geo = new Geocoder(this.getApplicationContext(), Locale.getDefault());
             List<Address> addresses = geo.getFromLocation(latLng.latitude, latLng.longitude, 1);
             if (addresses.isEmpty()) {
-                return "No available address.";
-            }
-            else {
+                return "Unknown Address";
+            } else {
                 if (addresses.size() > 0) {
-                    String[] addr=addresses.get(0).getAddressLine(0).split(",");
-                    return addr[0]+", "+addr[1];
+                    //String[] addr = addresses.get(0).getAddressLine(0).split(",");
+                    return addresses.get(0).getAddressLine(0);
                     //return addresses.get(0).getFeatureName() + ", " +addresses.get(0).getThoroughfare()+", "+ addresses.get(0).getLocality() +", " + addresses.get(0).getAdminArea();
                 }
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return "No available address.";
+        return "Unknown Address";
     }
 
     @Override
@@ -159,4 +289,7 @@ public class NewVolunteeringActivity extends AppCompatActivity implements OnMapR
         mMapView.onLowMemory();
     }
 
+    @OnClick(R.id.submit)
+    public void submit() {
+    }
 }
